273a9ca2896f3a4ef7e3a5beff20a97bd684ce56
LexSnow <108388996+LexSnow@users.noreply.github.com>
2023-08-16 14:20:13 +0200

    add api-gateway

---
+++ api-gateway/src/main/java/com/lex/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lex.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
